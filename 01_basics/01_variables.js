ac03ba6db062c7cebf44ac1e286ca5c8c12b2cfe
Rajdeep Singha <137903887+RajuC0der@users.noreply.github.com>
2023-12-28 09:21:13 +0530

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,8 +2,14 @@ const accountId = 144553 // can't be changed
 let accountEmail ="rajdeepsingha@gmail.com"
 var accountpassword= "12345"// can be changed
 accountCity ="West Bengal"
-
+let accountState;// it will be undefined as no value is assigned
 //accountID =2
 accountpassword="23456"
 console.log(accountId);
 console.log(accountpassword);
+
+/*
+preffer not to use var
+because of issue in block scope and function scope
+*/
+console.table([accountId, accountEmail,accountpassword,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+//alert(3+3)// we are using nodejs, not browser
+
+console.log(3+3) // code redeability should be our priority
+
+
+let name ="Rajdeep"
+let age =18
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+// string=> ""
+// boolean =>true/false
+// null => standalone value
+// undefined =>
+// symbol
+console.log(typeof age)
+
+// null ka type hai object

df6fec74a3b86d1abacc9ba85d3becafbb153355
Rajdeep Singha <137903887+RajuC0der@users.noreply.github.com>
2023-12-28 08:45:53 +0530

    questions

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553 // can't be changed
+let accountEmail ="rajdeepsingha@gmail.com"
+var accountpassword= "12345"// can be changed
+accountCity ="West Bengal"
+
+//accountID =2
+accountpassword="23456"
+console.log(accountId);
+console.log(accountpassword);

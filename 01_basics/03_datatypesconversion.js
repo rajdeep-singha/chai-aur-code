509af03c00350a99b56d38cd516248512e49d9ee
Rajdeep Singha <137903887+RajuC0der@users.noreply.github.com>
2023-12-28 12:45:59 +0530

    conversion of datatypes

---
+++ 01_basics/03_datatypesconversion.js
@@ -0,0 +1,22 @@
+let score ="33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=> 33
+// "33abc" => NaN
+// true =>; false =>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=> true; 0=> false 
+// "" => false
+//"Rajdeep"=> true
+
+

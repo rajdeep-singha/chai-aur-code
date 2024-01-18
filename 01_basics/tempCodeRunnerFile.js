a7622f02891254df1f3d20051d49948fc05c9e33
Rajdeep Singha <137903887+RajuC0der@users.noreply.github.com>
2024-01-18 10:51:57 +0530

    Day 7

---
+++ 01_basics/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+let userone={
+    email: "rajdeepsingha384@gmail.com",
+    upi:"rajdeep@ybl"
+}
+
+let usertwo = userone
+
+usertwo.email =" hitesh@google.com,"
+
+console.log(userone);
+console.log(usertwo);
